f48be9d882c3ab09c6be73fea38a87ae86b202c2
Shwetha Kulal <90205229+shwethajk@users.noreply.github.com>
2026-03-05 12:17:09 +0530

    Add imports for JavascriptExecutor and Duration

--- src/test/java/com/shwetha/pageobjects/HomePage.java
+++ src/test/java/com/shwetha/pageobjects/HomePage.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.JavascriptExecutor;
+import java.time.Duration;
 
 // import com.shwetha.framework.utils.WaitUtils;
 
